a41e92d751e7f30aef70b416e293b552340e51ee
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-07 19:18:15 +0530

    Create WordEmbedding.py.

---
+++ Word2vec/WordEmbedding.py
@@ -0,0 +1,16 @@
+from gensim.models import Word2Vec
+from nltk.tokenize import word_tokenize
+import nltk
+nltk.download('punkt')
+sentence=[
+    "Word embeddings are powerful tools in natural language processing.",
+    "Word2Vec is a popular algorithm for generating word embeddings.",
+    "It captures semantic relationships between words.",
+    "Sentiment analysis benefits from using word embeddings."
+]
+token=[word_tokenize(word.lower()) for word in sentence]
+model=Word2Vec(sentences=token,vector_size=100,window=5,min_count=1,workers=4)
+model.save("model")
+loaded=Word2Vec.load("model")
+vector=loaded.wv['word']
+print(vector)
