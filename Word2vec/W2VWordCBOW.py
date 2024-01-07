8c17d52ed20f55a2b94e0ba5dd479ef85b9ca390
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-07 20:39:00 +0530

    Helps to predict the output using CBOW

--- Word2vec/W2VWordCBOW.py
+++ Word2vec/W2VWordCBOW.py
@@ -3,7 +3,7 @@ from gensim.models import Word2Vec
 from nltk.tokenize import word_tokenize
 loaded_cbow_model = Word2Vec.load("CBOW_model")
 missing_word_sentence = f"I love __ language processing with {chosen_word}."
-tokenized_missing_word_sentence = word_tokenize(missing_word_sentence.lower())
+tokenized_missing_word_sentence = word_tokenize(missing_word_sentence.lower()) 
 missing_word_index = tokenized_missing_word_sentence.index("__")
 context_words = tokenized_missing_word_sentence[
     max(0, missing_word_index - 2) : missing_word_index
